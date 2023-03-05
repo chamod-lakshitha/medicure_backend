bf2abfe45b8f797c14ef58abebbf4d4b91c6a916
de silva <chamodlakshitha12@gmail.com>
2023-03-05 16:22:32 +0530

    user login and register has tested partially

--- __tests__/medicure_backend_test.js
+++ __tests__/medicure_backend_test.js
@@ -16,7 +16,19 @@ describe('register new user', () => {
     const res = await request(app)
       .post('/api/user/register')
       .send({ name: 'test2', email: 'test2@gmail.com', password: '123' });
+    expect(res.statusCode).toEqual(200);
     expect(res.body.success).toEqual(true);
     expect(res.body.alreadyRegistered).toEqual(true);
+  });
+});
+
+describe('user login with valid email and password', () => {
+  it('user should be able to login', async () => {
+    const res = await request(app)
+      .post('/api/user/login')
+      .send({ email: 'test@gmail.com', password: '123456' });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toEqual(true);
+    expect(res.body.userID).toEqual(2);
   });
 });
